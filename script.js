b3c2e45c340fcaa653824be2c0ba5005481d165d
Homem Tapete <103212335+lucascordeiro269@users.noreply.github.com>
2024-07-24 10:20:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,38 +6,38 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saiu do Jardim você se depara com um novo Adão, um Adão que consegue responder todas as dúvidas que uma pessoa pode ter, ele também ofrece a salvação, a vida eterna etc. Qual o primeiro pensamento?",
         alternativas: [
             "Isso é assustador!",
             "Isso é maravilhoso!"
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Com a descoberta deste novo Adão, chamado Jesus de Nazaré, uma professora de teologia da escola decidiu fazer uma sequência de aulas sobre Jesus. No fim de uma aula ela pede que você escreva um trabalho sobre quem era Jesus em sala de aula. Qual atitude você toma?",
         alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            "Utilizar a Bíblia como base de buscas  para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
             "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como Jesus transforma a vida das pessoas. Nesse debate, como você se posiciona?",
         alternativas: [
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            "Defende a ideia de que Jesus é o único que pode preencher o vazio que sentimos.",
+            "defenda que somos bons o suficiente e não precisamos de Jesus."
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre Jesus. E agora?",
         alternativas: [
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            "Desenhar uma imagem que representa o que Jesus é para você.",
             "Criar uma imagem utilizando um gerador de imagem de IA."
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+        enunciado: "Um colega de turma após estudar sobre Jesus, resolveu aceita-lo,ele te procura para saber como fazer isto, o que você faz?",
         alternativas: [
-           "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+           "instrui-lo com base nos ensinamentos biblicos, e leva-lo para o corpo de Jesus Cristo (igreja) que você faz parte.",
+            "Falar para ele ir buscar as informações na Bíblia e deixar ele se virar sozinho."
         ]
     },
 ];
