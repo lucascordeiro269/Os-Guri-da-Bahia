3dc9b7d4066f770fa8ac1cf31e86f3df79e769f8
geovanesanto <103213862+geovanesanto@users.noreply.github.com>
2024-10-16 09:39:40 -0300

    att

--- perguntas.js
+++ perguntas.js
@@ -1,39 +1,37 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu do Jardim, você se depara com um novo Adão, um Adão que consegue responder a todas as dúvidas que uma pessoa pode ter. Ele também oferece a salvação, a vida eterna, etc. Qual o primeiro pensamento?",
+        enunciado: "Após a expulsão do Jardim, você se depara com um novo Adão, um Adão que consegue responder a todas as dúvidas que uma pessoa pode ter. Ele também oferece a salvação, a vida eterna, etc. Qual o primeiro pensamento?",
         alternativas: [
             {
                 texto: "Isso é assustador!",
                 afirmacao: [
-                    "Somos chamados para uma missão muito importante, espalhar o plano de salvação por toda Terra.",
-                    "pescadores de homens."
+                    "Somos chamados para uma missão muito importante, espalhar o plano de salvação por toda Terra. (Nós temos sido maus e perversos; pecamos como nossos antepassados pecaram. - Salmos 106-6)."
                 ],
                 proxima: 1,
             },
             {
                 texto: "Isso é maravilhoso!",
                 afirmacao: [
-                    "Somos chamados para uma missão muito importante, espalhar o plano de salvação por toda Terra.",
-                    "Pescadores de homens."
+                    "Somos chamados para uma missão muito importante, espalhar o plano de salvação por toda Terra. (Nós temos sido maus e perversos; pecamos como nossos antepassados pecaram - Salmos 106-6)."
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "Com a descoberta deste novo Adão, chamado Jesus de Nazaré, uma professora de teologia da escola decidiu fazer uma sequência de aulas sobre Jesus. No fim de uma aula, ela pede que você escreva um trabalho sobre quem era Jesus. Qual atitude você toma?",
+        enunciado: "Com a descoberta deste novo Adão, chamado Jesus, uma professora de teologia da escola decidiu fazer uma sequência de aulas sobre Jesus. No fim de uma aula, ela pede que você escreva um trabalho sobre quem era Jesus. Qual atitude você toma?",
         alternativas: [
             {
                 texto: "Utilizar a Bíblia como base de busca para encontrar informações relevantes para o trabalho e explicar numa linguagem que facilite o entendimento.",
                 afirmacao: [
-                    "Na Bíblia encontramos respostas para tudo."
+                    "Na Bíblia encontramos respostas para tudo (Porque Deus amou o mundo tanto, que deu o seu único Filho, para que todo aquele que nele crer não morra, mas tenha vida eterna. - João 3-16)."
                 ],
                 proxima: 3,
             },
             {
                 texto: "Escrever o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
                 afirmacao: [
-                    "Na Bíblia encontramos respostas para tudo."
+                    "Na Bíblia encontramos respostas para tudo (Porque Deus amou o mundo tanto, que deu o seu único Filho, para que todo aquele que nele crer não morra, mas tenha vida eterna. - João 3-16)."
                 ],
                 proxima: 4,
             },
@@ -45,16 +43,14 @@ export const perguntas = [
             {
                 texto: "Defender a ideia de que Jesus é o único que pode preencher o vazio que sentimos.",
                 afirmacao: [
-                    "Jesus nos salvou.",
-                    "pecadores."
+                    "Jesus nos salvou (Ele nos salvou porque teve compaixão de nós, e não porque nós tivéssimos feito alguma coisa boa. Ele nos salvou por meio do Espírito Santo, que nos lavou, fazendo com que nacêssemos de novo e dando-nos uma nova vida. - Tito 3-5)."
                 ],
                 proxima: 3,
             },
             {
                 texto: "Defender que somos bons o suficiente e não precisamos de Jesus.",
                 afirmacao: [
-                    "Jesus nos salvou.",
-                    "pecadores."
+                    "Jesus nos salvou (Ele nos salvou porque teve compaixão de nós, e não porque nós tivéssimos feito alguma coisa boa. Ele nos salvou por meio do Espírito Santo, que nos lavou, fazendo com que nacêssemos de novo e dando-nos uma nova vida. - Tito 3-5)."
                 ],
                 proxima: 4,
             },
@@ -66,16 +62,14 @@ export const perguntas = [
             {
                 texto: "Instruí-lo com base nos ensinamentos bíblicos e levá-lo para o corpo de Jesus Cristo (igreja) do qual você faz parte.",
                 afirmacao: [
-                    "Devemos compartilhar Jesus",
-                    "não guarda-lo para nós mesmos."
+                    "Devemos compartilhar Jesus, não guarda-lo para nós mesmos. (Então Ele disse: Vão pelo mundo inteiro e anunciem o evangelho a todas as pessoas. Quem crer e for batizado será salvo, mas quem não crer será condenado. - Marcos 16-15:16.)"
                 ],
                 proxima: 5,
             },
             {
                 texto: "Falar para ele ir buscar as informações na Bíblia e deixá-lo se virar sozinho.",
                 afirmacao: [
-                    "Devemos compartilhar Jesus",
-                    "não guarda-lo para nós mesmos."
+                    "Devemos compartilhar Jesus, não guarda-lo para nós mesmos. (Então Ele disse: Vão pelo mundo inteiro e anunciem o evangelho a todas as pessoas. Quem crer e for batizado será salvo, mas quem não crer será condenado. - Marcos 16-15:16.)"
                 ],
                 proxima: 6,
             },
